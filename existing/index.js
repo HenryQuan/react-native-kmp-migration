6239ef3fe7a90796c31487879015b1b3f0dbde7b
Yiheng <development.henryquan@gmail.com>
2024-10-27 13:35:13 +1100

    maybe need to downgrade to 75

--- existing/index.js
+++ existing/index.js
@@ -3,7 +3,25 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Existing from './src/Existing';
+
+const Stack = createNativeStackNavigator();
+
+function Main() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={App} />
+        <Stack.Screen name="Existing" component={Existing} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ existing/src/Existing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Existing(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Just consider this as an existing screen</Text>
+    </View>
+  );
+}
+
+export default Existing;
